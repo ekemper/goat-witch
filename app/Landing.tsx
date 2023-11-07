ca3c447d1648ca66a710244ad2be76b0d73b6ed3
Edward Kemper <edwardkemper@gmail.com>
2023-11-07 09:05:41 -0700

    save

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -16,8 +16,8 @@ const Jumbotron = () => {
                 src={'/IMG_0302_transparent.png'} 
                 alt='Goat Witch' />
 
-            <div className="flex flex-col w-full my-16">
-                <h3 className="text-white text-center">{description}</h3>
+            <div className="flex flex-col w-full my-10">
+                <h3 className="m-6 text-white text-center">{description}</h3>
                 <Button className="mx-auto black uppercase">{callToAction}</Button>
             </div>
 
@@ -31,9 +31,10 @@ const Landing = () => {
             <video 
                 src={videoSource} 
                 autoPlay muted loop 
-                className="video-bg max-w-7xl md:max-w-full absolute" 
+                className="hidden lg:block video-bg max-w-7xl md:max-w-full absolute" 
                 // playbackRate={0.5}  // TODO: EK WTF?
                  />
+            <img src={'/mob-bg.jpg'} className="lg:hidden"/>     
             {/* <div className="bg-overlay"> */}
                 <Jumbotron />
             {/* </div> */}
