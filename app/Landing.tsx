36a6d03b83d1a7aa5c1eda37a97ec4d827afb881
Edward Kemper <edwardkemper@gmail.com>
2023-11-09 00:16:50 -0700

    landing on desktop

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Image from "next/image";
 
 const videoSource = '/trees.mp4';
 const description = 'Dope Human'
@@ -8,7 +6,6 @@ const callToAction = 'Book a session'
 
 const Jumbotron = () => {
 
-    // TODO EK: why does the hover color not work??
     const callToActionClasses = `
         w-3/4 
         mx-auto 
@@ -16,18 +13,29 @@ const Jumbotron = () => {
         bg-green-900
         text-white
         uppercase
-        p-2`
+        p-2
+        md:w-1/2`
+
+    const JumbotronContainerDesktopClasses = `
+        md:flex-row
+        md:relative 
+        md:max-w-6xl
+        md:mx-auto
+        `
 
     return (
-        <div className='p-4 md:px-30 flex-col md:flex-row'>
+        <div className={`p-4 flex flex-col ${JumbotronContainerDesktopClasses}`}>
 
             {/* eslint-disable-next-line @next/next/no-img-element*/}
-            <img 
-                className="w-full md:w-1/2" 
-                src={'/IMG_0302_transparent.png'} 
-                alt='Goat Witch'/>
+            <img
+                className="w-full md:w-1/2"
+                src={'/IMG_0302_transparent.png'}
+                alt='Goat Witch Tatoo' />
 
-            <div className="flex flex-col w-full my-10">
+            <div className={`
+                flex flex-col w-full my-10 
+                md:w-1/2 md:grid md:content-center md:my-0
+                `}>
                 <h3 className="m-6 text-white text-center">{description}</h3>
                 <button className={callToActionClasses}>{callToAction}</button>
             </div>
@@ -39,15 +47,15 @@ const Jumbotron = () => {
 const Landing = () => {
     return (
         <div className="h-screen grid content-center  md:max-w-full">
-            <video 
-                src={videoSource} 
-                autoPlay muted loop 
-                className="hidden lg:block video-bg max-w-7xl md:max-w-full absolute" 
-                // playbackRate={0.5}  // TODO: EK WTF?
-                 />
+            <video
+                src={videoSource}
+                autoPlay muted loop
+                className="hidden lg:block video-bg max-w-7xl md:max-w-full absolute"
+            // playbackRate={0.5}  // TODO: EK WTF?
+            />
 
             {/* <div className="bg-overlay"> */}
-                <Jumbotron />
+            <Jumbotron />
             {/* </div> */}
         </div>
     )
