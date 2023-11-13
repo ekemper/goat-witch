33e3990da77180e4aec0f35e7d54bae99f48d632
Edward Kemper <edwardkemper@gmail.com>
2023-11-13 12:09:33 -0700

    save

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,13 +5,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-
-const expand = 'sm'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function NavHeader() {
   return (
     <>
-      <Navbar expand={expand} variant='dark' >
+      <Navbar expand='sm' variant='dark' >
         <Container fluid>
           <Navbar.Toggle className='color-green-900' aria-controls={'offcanvasNavbar-expand'} />
           <Navbar.Offcanvas
@@ -23,11 +22,16 @@ function NavHeader() {
             </Offcanvas.Header>
             <Offcanvas.Body className='h-auto'>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link className='text-white my-auto' href="#action1">Booking</Nav.Link>
+
+              <li><Link to="/#about">About</Link></li>
+              <li><Link to="/#top">top</Link></li>
+
+                {/* <Nav.Link className='text-white my-auto' href="#action1">Booking</Nav.Link>
                 <Nav.Link className='text-white my-auto' href="#action2">Portfolio</Nav.Link>
                 <Nav.Link className='text-white my-auto' href="#action2">Flash</Nav.Link>
-                <Nav.Link className='text-white my-auto' href="#action2">About</Nav.Link>
+                <Nav.Link className='text-white my-auto' href="#action2">About</Nav.Link> */}
                 {/* eslint-disable-next-line @next/next/no-img-element */}
+
                 <img 
                   className="h-12" 
                   src={'/Millennium_Falcon_transparent.png'} 
