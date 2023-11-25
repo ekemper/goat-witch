c5376fa009485e06be7fca2ffbf825a7b5e233fd
Edward Kemper <edwardkemper@gmail.com>
2023-11-25 03:01:44 -0700

    broken calendly

--- src/App.js
+++ src/App.js
@@ -4,23 +4,45 @@ import Header from './Header';
 import About from './About';
 import Gallery from './Gallery';
 import Footer from './Footer';
-import TailwingWidthGuide from "./TailwindWidthGuide"
+// import TailwingWidthGuide from "./TailwindWidthGuide"
 import Booking from './Booking';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+window.onload = function () {
+
+  const options = {
+    url: 'https://calendly.com/edwardkemper',
+    text: 'Schedule time with me',
+    color: '#0069ff',
+    textColor: '#ffffff',
+    branding: true
+  }
+
+
+  setTimeout(() => {
+    Calendly.initBadgeWidget(options);
+    console.log('called')
+  }, 10000)
+}
 
 function App() {
 
   return (
     <main className="bg-grayGreenBackground">
+      <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet"></link>
+      <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
       {/* <TailwingWidthGuide /> */}
       <Header />
       <Landing />
+      <Booking />
       <About />
       <Gallery />
-      <Booking />
       <Footer />
     </main>
   );
 }
 
 export default App;
+
+
