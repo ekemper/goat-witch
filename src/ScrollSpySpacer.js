7e3e27c481e5bc6ec93e456e89cf82b77bd0c638
Edward Kemper <edwardkemper@gmail.com>
2023-12-09 11:34:49 -0700

    neg margins for section dividers

--- src/ScrollSpySpacer.js
+++ src/ScrollSpySpacer.js
@@ -1,4 +1,7 @@
 
 export default function ScrollSpySpacer() {
-    return <div className="h-12 sm:h-24 "><img src={"/assets/sectionDivider.png"} /></div>
+    const negMargins = `-mt-3 sm:-mt-6 md:-mt-8 lg:-mt-10 xl:-mt-14`
+    return <div className={`h-12 sm:h-24 absolute ${negMargins}`}>
+    <img src={"/assets/sectionDivider.png"} />
+    </div>
 }
