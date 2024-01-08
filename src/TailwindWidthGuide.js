8f9bc12cacad66f0cd11a547dadb88c30fb0fc53
Edward Kemper <edwardkemper@gmail.com>
2024-01-08 09:41:42 -0700

    working menu!

--- src/TailwindWidthGuide.js
+++ src/TailwindWidthGuide.js
@@ -1,6 +1,6 @@
 const TailwingWidthGuide = () => {
     return (
-        <div className="flex flex-col text-right text-white fixed z-10">
+        <div className="absolute text-right z-20">
             <h1 className="hidden xs:block mx-4">XS</h1>
             <h1 className="hidden sm:block mx-4">SM</h1>
             <h1 className="hidden md:block mx-4">MD</h1>
